5fc383340f5ac4943a12535d208feda3f1bf449f
Paul Morie <pmorie@gmail.com>
2014-09-16 11:55:46 -0400

    Deployment Triggers POC
    Adds DeploymentTriggerController and DeploymentConfigController and
    fleshes out the deployments model.

--- pkg/cmd/master/master.go
+++ pkg/cmd/master/master.go
@@ -45,7 +45,8 @@ import (
 	osclient "github.com/openshift/origin/pkg/client"
 	"github.com/openshift/origin/pkg/cmd/flagtypes"
 	"github.com/openshift/origin/pkg/cmd/util/docker"
-	"github.com/openshift/origin/pkg/deploy"
+	deploycontroller "github.com/openshift/origin/pkg/deploy/controller"
+	deploygen "github.com/openshift/origin/pkg/deploy/generator"
 	deployregistry "github.com/openshift/origin/pkg/deploy/registry/deploy"
 	deployconfigregistry "github.com/openshift/origin/pkg/deploy/registry/deployconfig"
 	deployetcd "github.com/openshift/origin/pkg/deploy/registry/etcd"
@@ -181,6 +182,8 @@ func (c *config) startAllInOne() {
 	c.runReplicationController()
 	c.runBuildController()
 	c.runDeploymentController()
+	c.runDeploymentConfigController()
+	c.runDeploymentTriggerController()
 
 	select {}
 }
@@ -192,6 +195,8 @@ func (c *config) startMaster() {
 	c.runReplicationController()
 	c.runBuildController()
 	c.runDeploymentController()
+	c.runDeploymentConfigController()
+	c.runDeploymentTriggerController()
 
 	select {}
 }
@@ -224,23 +229,25 @@ func (c *config) runApiserver() {
 		glog.Errorf("Error setting up Kubernetes server storage: %v", err)
 	}
 
-	buildRegistry := buildetcd.New(etcdHelper)
-	imageRegistry := imageetcd.New(etcdHelper)
+	buildEtcd := buildetcd.New(etcdHelper)
+	imageEtcd := imageetcd.New(etcdHelper)
 	deployEtcd := deployetcd.New(etcdHelper)
 	routeEtcd := routeetcd.New(etcdHelper)
+	deployConfigGen := deploygen.NewDeploymentConfigGenerator(deployEtcd, deployEtcd, imageEtcd)
 
 	// initialize OpenShift API
 	storage := map[string]apiserver.RESTStorage{
-		"builds":                  buildregistry.NewREST(buildRegistry),
-		"buildConfigs":            buildconfigregistry.NewREST(buildRegistry),
-		"buildLogs":               buildlogregistry.NewREST(buildRegistry, kubeClient, proxyPrefix),
-		"images":                  image.NewREST(imageRegistry),
-		"imageRepositories":       imagerepository.NewREST(imageRegistry),
-		"imageRepositoryMappings": imagerepositorymapping.NewREST(imageRegistry, imageRegistry),
-		"deployments":             deployregistry.NewREST(deployEtcd),
-		"deploymentConfigs":       deployconfigregistry.NewREST(deployEtcd),
-		"templateConfigs":         template.NewStorage(),
-		"routes":                  routeregistry.NewREST(routeEtcd),
+		"builds":                    buildregistry.NewREST(buildEtcd),
+		"buildConfigs":              buildconfigregistry.NewREST(buildEtcd),
+		"buildLogs":                 buildlogregistry.NewREST(buildEtcd, kubeClient, proxyPrefix),
+		"images":                    image.NewREST(imageEtcd),
+		"imageRepositories":         imagerepository.NewREST(imageEtcd),
+		"imageRepositoryMappings":   imagerepositorymapping.NewREST(imageEtcd, imageEtcd),
+		"deployments":               deployregistry.NewREST(deployEtcd),
+		"deploymentConfigs":         deployconfigregistry.NewREST(deployEtcd),
+		"generateDeploymentConfigs": deploygen.NewREST(deployConfigGen, v1beta1.Codec),
+		"templateConfigs":           template.NewStorage(),
+		"routes":                    routeregistry.NewREST(routeEtcd),
 	}
 
 	osMux := http.NewServeMux()
@@ -449,8 +456,18 @@ func (c *config) runDeploymentController() {
 	kubeClient := c.getKubeClient()
 	osClient := c.getOsClient()
 
-	deployController := deploy.NewDeploymentController(kubeClient, osClient, env)
+	deployController := deploycontroller.NewDeploymentController(kubeClient, osClient, env)
 	deployController.Run(10 * time.Second)
+}
+
+func (c *config) runDeploymentConfigController() {
+	deployConfigController := deploycontroller.NewDeploymentConfigController(c.getOsClient())
+	deployConfigController.Run(30 * time.Second)
+}
+
+func (c *config) runDeploymentTriggerController() {
+	deployTriggerController := deploycontroller.NewDeploymentTriggerController(c.getOsClient())
+	deployTriggerController.Run(30 * time.Second)
 }
 
 func env(key string, defaultValue string) string {
---
+++ pkg/deploy/generator/config_generator.go
@@ -0,0 +1,133 @@
+package generator
+
+import (
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api/errors"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
+	"github.com/golang/glog"
+	deployapi "github.com/openshift/origin/pkg/deploy/api"
+	deployregistry "github.com/openshift/origin/pkg/deploy/registry/deploy"
+	deployconfig "github.com/openshift/origin/pkg/deploy/registry/deployconfig"
+	deployutil "github.com/openshift/origin/pkg/deploy/util"
+	imageapi "github.com/openshift/origin/pkg/image/api"
+	imagerepo "github.com/openshift/origin/pkg/image/registry/imagerepository"
+)
+
+type DeploymentConfigGenerator interface {
+	Generate(deploymentConfigID string) (*deployapi.DeploymentConfig, error)
+}
+
+type deploymentConfigGenerator struct {
+	deploymentRegistry   deployregistry.Registry
+	deployConfigRegistry deployconfig.Registry
+	imageRepoRegistry    imagerepo.Registry
+}
+
+func NewDeploymentConfigGenerator(deploymentRegistry deployregistry.Registry,
+	deployConfigRegistry deployconfig.Registry, imageRepoRegistry imagerepo.Registry) DeploymentConfigGenerator {
+	return &deploymentConfigGenerator{
+		deploymentRegistry:   deploymentRegistry,
+		deployConfigRegistry: deployConfigRegistry,
+		imageRepoRegistry:    imageRepoRegistry,
+	}
+}
+
+func (g *deploymentConfigGenerator) Generate(deploymentConfigID string) (*deployapi.DeploymentConfig, error) {
+	glog.Infof("Generating new deployment config from deploymentConfig %v", deploymentConfigID)
+
+	deploymentConfig, err := g.deployConfigRegistry.GetDeploymentConfig(deploymentConfigID)
+	if err != nil {
+		glog.Errorf("Error getting deploymentConfig for id %v", deploymentConfigID)
+		return nil, err
+	}
+
+	deploymentID := deployutil.LatestDeploymentIDForConfig(deploymentConfig)
+
+	deployment, err := g.deploymentRegistry.GetDeployment(deploymentID)
+	if err != nil && !errors.IsNotFound(err) {
+		glog.Errorf("Error getting deployment: %#v", err)
+		return nil, err
+	}
+
+	configPodTemplate := deploymentConfig.Template.ControllerTemplate.PodTemplate
+
+	referencedRepoNames := referencedRepoNames(deploymentConfig)
+	referencedRepos := imageReposByDockerImageRepo(g.imageRepoRegistry, referencedRepoNames)
+
+	for _, repoName := range referencedRepoNames.List() {
+		params := deployutil.ParamsForImageChangeTrigger(deploymentConfig, repoName)
+		repo, ok := referencedRepos[params.RepositoryName]
+		if !ok {
+			return nil, fmt.Errorf("Config references unknown ImageRepository '%s'", params.RepositoryName)
+		}
+
+		// TODO: If the tag is missing, what's the correct reaction?
+		tag, tagExists := repo.Tags[params.Tag]
+		if !tagExists {
+			glog.Errorf("No tag %s found for repository %s (potentially invalid DeploymentConfig state)", tag, repoName)
+			continue
+		}
+		newImage := repo.DockerImageRepository + ":" + tag
+
+		containersToCheck := util.NewStringSet(params.ContainerNames...)
+		for i, container := range configPodTemplate.DesiredState.Manifest.Containers {
+			if !containersToCheck.Has(container.Name) {
+				continue
+			}
+
+			// TODO: If we grow beyond this single mutation, diffing hashes of
+			// a clone of the original config vs the mutation would be more generic.
+			if newImage != container.Image {
+				configPodTemplate.DesiredState.Manifest.Containers[i].Image = newImage
+			}
+		}
+	}
+
+	if deployment == nil {
+		if deploymentConfig.LatestVersion == 0 {
+			// TODO: Is this a safe assumption? -- NO
+			deploymentConfig.LatestVersion = 1
+		}
+	} else if !deployutil.PodTemplatesEqual(configPodTemplate, deployment.ControllerTemplate.PodTemplate) {
+		deploymentConfig.LatestVersion += 1
+	}
+
+	return deploymentConfig, nil
+}
+
+func imageReposByDockerImageRepo(registry imagerepo.Registry, filter *util.StringSet) map[string]imageapi.ImageRepository {
+	repos := make(map[string]imageapi.ImageRepository)
+
+	imageRepos, err := registry.ListImageRepositories(labels.Everything())
+	if err != nil {
+		glog.Errorf("Error listing imageRepositories: %#v", err)
+		return repos
+	}
+
+	for _, repo := range imageRepos.Items {
+		if filter.Has(repo.DockerImageRepository) {
+			repos[repo.DockerImageRepository] = repo
+		}
+	}
+
+	return repos
+}
+
+// Returns the image repositories names a config has triggers registered for
+func referencedRepoNames(config *deployapi.DeploymentConfig) *util.StringSet {
+	repoIDs := &util.StringSet{}
+
+	if config == nil || config.Triggers == nil {
+		return repoIDs
+	}
+
+	for _, trigger := range config.Triggers {
+		if trigger.Type == deployapi.DeploymentTriggerOnImageChange {
+			repoIDs.Insert(trigger.ImageChangeParams.RepositoryName)
+		}
+	}
+
+	return repoIDs
+}
