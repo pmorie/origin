95bd966fe1125c37744d79e707afc150300fdd2a
Dan Mace <ironcladlou@gmail.com>
2014-09-25 16:52:54 -0400

    Add DeployConfig generator and endpoint

--- pkg/deploy/config_generator.go
+++ pkg/deploy/config_generator.go
@@ -1,50 +1,34 @@
 package deploy
 
 import (
-	"fmt"
-	"strings"
-	"time"
-
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/util"
-	"github.com/GoogleCloudPlatform/kubernetes/pkg/watch"
 	"github.com/golang/glog"
 	osclient "github.com/openshift/origin/pkg/client"
 	deployapi "github.com/openshift/origin/pkg/deploy/api"
-	imageapi "github.com/openshift/origin/pkg/image/api"
 )
 
 type DeploymentGenerator struct {
 	osClient osclient.Client
 }
 
-func (g *DeploymentGenerator) generateDeployment(config *deployapi.DeploymentConfig) (*deployapi.Deployment, error) {
-	glog.Infof("Generating deployment for deploymentConfig %v", config.ID)
+func (g *DeploymentGenerator) generateDeployment(deploymentConfigID string) (*deployapi.DeploymentConfig, error) {
+	glog.Infof("Generating deploymentConfig %v", deploymentConfigID)
 
-	labels := make(map[string]string)
-	for label, value := range config.Labels {
-		labels[label] = value
-	}
-	labels["configID"] = config.ID
+	config, err = g.osClient.GetDeploymentConfig(deploymentConfigID)
 
-	deployment := &deployapi.Deployment{
-		JSONBase:           api.JSONBase{},
-		Labels:             labels,
-		Strategy:           config.Template.Strategy,
-		ControllerTemplate: config.Template.ControllerTemplate,
+	if err != nil {
+		return nil, err
 	}
 
 	for _, repoName := range referencedRepos(config).List() {
 		params := paramsForImageChangeTrigger(config, repoName)
 		repo := g.imageRepoCache.cachedRepo(repoName)
 
-		for _, container := range deployment.ControllerTemplate.PodTemplate.DesiredState.Manifest.Containers {
+		for _, container := range config.Template.ControllerTemplate.PodTemplate.DesiredState.Manifest.Containers {
 			if container.Image == params.ImageName {
 				container.Image = repoName + ":" + repo.Tags[params.Tag]
 			}
 		}
 	}
 
-	return deployment, nil
+	return config, nil
 }
