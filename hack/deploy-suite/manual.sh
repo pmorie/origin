65174de78ed172cbe18724e24fa98641ea502a4b
pweil- <pweil@redhat.com>
2014-09-30 15:54:05 -0400

    newlines, get the deploy config from the generator, updates to cleanup code

--- hack/deploy-suite/manual.sh
+++ hack/deploy-suite/manual.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
+#
+# Test case for manual deployments
+#
+
 # Exit on error
 set -e
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG_FILE=${SCRIPT_PATH}/manual.json
+EXPECTED_ID="frontend-config3"
+TEMP_CONFIG_FILE_NAME="temp_deploy_config.json"
 
 function teardown() {
-    echo "tear down..remove the deployments and clean up"
+    openshift kube delete deploymentConfigs/${EXPECTED_ID}
+    rm ${SCRIPT_PATH}/temp_deploy_config.json
 }
 
+function validate() {
+    if [ "${1}" != "${2}" ]; then
+        echo "FAILURE: Expected a ${3} with ${2} but found {$1}"
+        exit 1
+    fi
+}
 trap "teardown" EXIT
 
 # post the deployment
 openshift kube create deploymentConfigs -c ${CONFIG_FILE}
 
-# verify
-openshift kube list deploymentConfigs
-openshift kube list
+# verify the config was created and has the correct trigger
+DEPLOY_CONFIG=$(openshift kube --template="{{.ID}}" get deploymentConfigs/${EXPECTED_ID} | tr -d ' ')
+
+#TODO: DEPLOY_CONFIG_TRIGGER=$(openshift kube --template="{{.Triggers}}" get deploymentConfigs/${EXPECTED_ID} | tr -d ' ')
+
+validate ${DEPLOY_CONFIG} ${EXPECTED_ID} "deployment config"
+
+# get the config (on the generator)  endpoint for generated config is /genDeploymentConfigs
+CONFIG=$(openshift kube --json get genDeploymentConfigs/${EXPECTED_ID})
+echo ${CONFIG} > ${SCRIPT_PATH}/${TEMP_CONFIG_FILE_NAME}
+
+#verify
+
+# put on deployment config
 
 exit 0
--- hack/test-deploy.sh
+++ hack/test-deploy.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+#
+# Starts an openshift all in one cluster and runs the deploy test suite, then shuts down
+#
+
+# Exit on error
 set -e
 
-SUITE_DIR=$(ls $(dirname $0)/deploy-suite/*.sh)
+# use a configured ip address for containers that are not on the localhost (aka vagrant)
+# TODO: ensure openshift and kubernetes are started with the correct ip and commands work in vagrant
+IP_ADDRESS=${1:-127.0.0.1}
+# option to leave openshift up after testing in case you need to query it after the tests
+LEAVE_UP=${2:-0}
+
+TEST_SUITES=$(ls $(dirname $0)/deploy-suite/*.sh)
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Add openshift to the path
 PATH=${SCRIPT_PATH}/../_output/go/bin:$PATH
 
 source ${SCRIPT_PATH}/util.sh
@@ -13,16 +26,22 @@ source ${SCRIPT_PATH}/util.sh
 openshift start &
 OPENSHIFT_PID=$!
 
-# Wait for server to start
-wait_for_url "http://127.0.0.1:8080/healthz" "apiserver: "
-
-# Do cluster setup
+# Wait for server to start. Not sure if this is actually working
+wait_for_url "http://${IP_ADDRESS}:8080/healthz" "apiserver: "
 
 set +e
 
-echo ${SUITE_DIR}
+function test-teardown() {
+    echo "tearing down suite"
+    kill ${OPENSHIFT_PID}
+}
+
+if [[ ${LEAVE_UP} -ne 1 ]]; then
+  trap test-teardown EXIT
+fi
+
 # Run tests
-for test_file in ${SUITE_DIR}; do
+for test_file in ${TEST_SUITES}; do
     "${test_file}"
     result="$?"
 
@@ -37,7 +56,5 @@ done
 if [[ ${any_failed} -ne 0 ]]; then
   echo "At least one test failed."
 fi
-
-kill ${OPENSHIFT_PID}
 
 exit ${any_failed}
