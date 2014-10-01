6316673a0cdd8a9d4581519142fe3b0fa1ec511c
Paul Morie <pmorie@gmail.com>
2014-10-01 18:41:48 -0400

    WIP: manual test

--- hack/deploy-suite/manual.sh
+++ hack/deploy-suite/manual.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #
 # Test case for manual deployments
@@ -9,40 +9,53 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+OS_API=$1
+
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-FIXTURE_PATH=${SCRIPT_PATH}/deploy-suite/fixtures
+FIXTURE_PATH=${SCRIPT_PATH}/fixtures
 EXPECTED_ID="hello-deployment-config"
 TEMP_CONFIG_FILE_NAME="temp_deploy_config.json"
 
 function teardown() {
-    openshift kube delete deploymentConfigs/${EXPECTED_ID}
-    rm ${SCRIPT_PATH}/temp_deploy_config.json
+  openshift kube -h $OS_API delete deploymentConfigs/${EXPECTED_ID}
+  rm ${SCRIPT_PATH}/temp_deploy_config.json
 }
 
 function validate() {
-    if [ "${1}" != "${2}" ]; then
-        echo "FAILURE: Expected a ${3} with ${2} but found {$1}"
-        exit 1
-    fi
+  if [ "${1}" != "${2}" ]; then
+    echo "FAILURE: Expected a ${3} with ${2} but found {$1}"
+    exit 1
+  fi
 }
 trap "teardown" EXIT
 
 # post the deployment
-openshift kube create deploymentConfigs -c ${FIXTURE_PATH}/manual.json
+openshift kube -h $OS_API create deploymentConfigs -c ${FIXTURE_PATH}/manual.json > /dev/null
+echo "posted deployment"
 
 # verify the config was created and has the correct trigger
-DEPLOY_CONFIG=$(openshift kube --template="{{.ID}}" get deploymentConfigs/${EXPECTED_ID} | tr -d ' ')
-
+DEPLOY_CONFIG=$(openshift kube -h $OS_API --template="{{.ID}}" get deploymentConfigs/${EXPECTED_ID} | tr -d ' ')
+validate ${DEPLOY_CONFIG} ${EXPECTED_ID} "deployment config"
 #TODO: DEPLOY_CONFIG_TRIGGER=$(openshift kube --template="{{.Triggers}}" get deploymentConfigs/${EXPECTED_ID} | tr -d ' ')
 
-validate ${DEPLOY_CONFIG} ${EXPECTED_ID} "deployment config"
-
-# get the config (on the generator)  endpoint for generated config is /genDeploymentConfigs
-CONFIG=$(openshift kube --json get genDeploymentConfigs/${EXPECTED_ID})
+# get the config (on the generator) endpoint for generated config is /genDeploymentConfigs
+CONFIG=$(openshift kube -h $OS_API --json get genDeploymentConfigs/${EXPECTED_ID})
 echo ${CONFIG} > ${SCRIPT_PATH}/${TEMP_CONFIG_FILE_NAME}
+openshift kube update -h $OS_API deploymentConfigs/hello-deployment-config -c ${SCRIPT_PATH}/${TEMP_CONFIG_FILE_NAME}
 
-#verify
+count=5
+status=''
+while [ $count -gt 0 ]; do
+  count=count-1
+  status=$(openshift kube -h $OS_API --template="{{.Status}}" get deployments/hello-deployment-config-1)
+  echo "Deployment status: $status"
+  if [ status == "complete" ]; then
+  	break
+  fi
+done
 
-# put on deployment config
+if [ "$status" -ne "complete" ]; then
+  exit 1
+fi
 
 exit 0
