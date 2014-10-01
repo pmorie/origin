5af0f75c2dc331c7d1862018b02daee974f81dc4
pweil- <pweil@redhat.com>
2014-09-30 21:43:51 -0400

    use fixture path, comments for steps discussed in call, initial implementation of docker registry

--- hack/test-deploy.sh
+++ hack/test-deploy.sh
@@ -16,18 +16,61 @@ LEAVE_UP=${2:-0}
 TEST_SUITES=$(ls $(dirname $0)/deploy-suite/*.sh)
 
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FIXTURE_PATH=${SCRIPT_PATH}/deploy-suite/fixtures
 
 # Add openshift to the path
 PATH=${SCRIPT_PATH}/../_output/go/bin:$PATH
 
 source ${SCRIPT_PATH}/util.sh
 
-# Start openshift
+# precondition: image will be built to openshift/registry image contains the registry
+# TODO: ensure docker is running
+# TODO: create /tmp/registry if it doesn't exist since the bootstrap-config.json needs it??
+#
+#1. start openshift
+#2. apply bootstrap config
+#  a. registry service and pod
+#  b. image repository records
+#3. tag openshift/hello-openshift into the registry
+#4. push to registry
+#5. apply the test config
+#  a. service
+#  b. deploymentConfig
+#6. check to see that a new deployment is created <configname>-0
+#7. query the status of the deployment, it should -> complete
+#8. query the status of the replication controller
+#9. query the deployed service (curl)
+
+
+#1. start openshift
 openshift start &
 OPENSHIFT_PID=$!
 
 # Wait for server to start. Not sure if this is actually working
 wait_for_url "http://${IP_ADDRESS}:8080/healthz" "apiserver: "
+
+#2. apply bootstrap config
+#  a. registry service and pod
+#  b. image repository records
+openshift kube apply -c ${FIXTURE_PATH}/bootstrap-config.json
+
+# TODO: how to verify it is up and ready (with list pods status?)
+
+#3. tag openshift/hello-openshift into the registry
+docker tag ncdc/openshift-registry localhost:5000/ncdc/openshift-registry
+#4.
+docker push localhost:5000/ncdc/openshift-registry
+
+#### here below goes into the manual test case
+#5. service & deployment config - see bootstrap & manual.json
+
+#TODO exiting here for testing purposes to ensure docker items are good
+exit
+
+#############################
+# WIP: RUNNING THE TESTS - STILL WORKING ON GETTING BOOTSTRAP UP
+#############################
+
 
 set +e
 
