e741b5982b1e7966b169b094f2fc673c89cd0b0f
Paul Morie <pmorie@gmail.com>
2014-10-02 14:56:20 -0400

    WIP: image change test

--- hack/deploy-suite/util.sh
+++ hack/deploy-suite/util.sh
@@ -38,9 +38,28 @@ function assert_at_least_one_replica() {
   fi
 }
 
+function assert_zero_replicas() {
+  id=$1
+  replicas=0
+  for (( i=1 ; i<=50 ; i++ )) ; do
+    replicas=$(openshift kube -h $OS_API --template="{{with index .Items 0}}{{.CurrentState.Replicas}}{{end}}" list replicationControllers -l deploymentID=hello-deployment-config-1)
+    echo "ReplicationController replicas: $replicas"
+    if [ "$replicas" -eq 0 ]; then
+      break
+    fi
+    sleep 5
+  done
+
+  if [ $replicas -ne 0 ]; then
+    echo "Failed!"
+    exit 255
+  fi
+}
+
 function assert_equals() {
   if [ "${1}" != "${2}" ]; then
     echo "FAILURE: Expected a ${3} with ${2} but found {$1}"
     exit 1
   fi
 }
+
